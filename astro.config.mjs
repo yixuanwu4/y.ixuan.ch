9c45236f04b31e386224500f3abee491709b07c2
Yixuan Wu <y@ixuan.ch>
2025-05-21 11:37:39 +0000

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: true
-      },
+  server: {
+    host: true
+  },
+  site: 'https://y.ixuan.ch',
 });
