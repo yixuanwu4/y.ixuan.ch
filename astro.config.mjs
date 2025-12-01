f57eb5dc4a614e8b8210132959b03f3e40d1084a
Yixuan <y+github@ixuan.ch>
2025-12-01 10:42:10 +0000

    chore: use unplugin-icons for contact icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import icon from 'astro-icon';
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true
   },
-
   site: 'https://y.ixuan.ch',
-  integrations: [icon()],
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
 });
