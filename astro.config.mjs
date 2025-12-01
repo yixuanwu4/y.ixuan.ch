f38e48efe51af6653bd6df02430fa87d1bdd6ab9
Yixuan <y+github@ixuan.ch>
2025-12-01 09:37:03 +0000

    chore: add astro icon dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: true
   },
+
   site: 'https://y.ixuan.ch',
+  integrations: [icon()],
 });
